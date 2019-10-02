c2c2fd9b9df25d33da8d7a95b3c4eb1744d3a8af
inthirakumaaran <inthira.14@cse.mrt.ac.lk>
2019-10-02 19:08:40 +0530

    basic dashboard created

---
+++ appserver/static/multiselect.js
@@ -0,0 +1,30 @@
+require([
+    'jquery',
+    'underscore',
+    'splunkjs/mvc',
+    'splunkjs/mvc/simplexml/ready!'
+    ],
+    function ($,_,mvc){
+
+        function multiselect(id){
+            var multi1 = mvc.Components.get(id)
+            multi1.on("change",function(){
+                current_val = multi1.val()
+                console.log("Current Vals: " + current_val)
+                var first_choice_value = multi1.options.choices[0].value;
+                if (current_val.length > 1 && current_val.indexOf(first_choice_value) == 0) {
+                    multi1.val(_.without(current_val, first_choice_value));
+                }
+                if (current_val.length > 1 && current_val.indexOf(first_choice_value) > 0) {
+                    multi1.val([first_choice_value]);
+                }
+            })
+        }
+        var all_multi_selects = document.getElementsByClassName("input-multiselect");
+        for (j = 0; j < all_multi_selects.length; j++) {
+            multiselect(all_multi_selects[j].id);
+        }
+       
+    }
+    
+    )
